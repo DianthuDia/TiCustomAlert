874b455c23c037dfa7ca3e123fe4dde4d580fbe1
DianthuDia <dianthudia@y7mail.com>
2013-02-27 21:14:51 +0900

    first import

---
+++ Classes/TkDmapTiCustomalertModuleAssets.h
@@ -0,0 +1,9 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+
+@interface TkDmapTiCustomalertModuleAssets : NSObject
+{
+}
+- (NSData*) moduleAsset;
+@end
---
+++ Classes/TkDmapTiCustomalertModuleAssets.m
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "TkDmapTiCustomalertModuleAssets.h"
+
+extern NSData* filterDataInRange(NSData* thedata, NSRange range);
+
+@implementation TkDmapTiCustomalertModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end
